7ac78cabac7d14fe845d12d20ed137c5d1d827b3
Rui Paulo <ruipaulo.unify@outlook.fr>
2023-10-18 19:15:27 +0200

    it's over for today

--- cursus/get_next_line/get_next_line copy.c
+++ cursus/get_next_line/get_next_line copy.c
@@ -1,18 +1,125 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line copy.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rude-jes <ruipaulo.unif@outlook.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 15:38:17 by rude-jes          #+#    #+#             */
-/*   Updated: 2023/10/18 18:29:48 by rude-jes         ###   ########.fr       */
+/*   Updated: 2023/10/18 19:15:00 by rude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+/*
+* Returns a pointer to an extend malloc
+* Copies bytes from ptr to the returned malloc
+* ft_exallocf frees the ptr's pointed malloc
+*
+* Size inputs represents the size of the malloc in bytes
+* If ptr is null, returns a new 'calloc' with newsize
+* Returns 0 if allocation failed and free ptr
+*/
+#include <stdio.h>
+void	*ft_exallocf(void *ptr, size_t size, size_t newsize)
+{
+	void	*p;
+	void	*pwriter;
+
+	p = malloc(newsize);
+	if (!p)
+	{
+		free(ptr);
+		return (0);
+	}
+	pwriter = p;
+	if (!ptr)
+	{
+		while ((size_t)pwriter++ - (size_t)p < newsize)
+			*((unsigned char *)(pwriter - 1)) = 0;
+		return (p);
+	}
+	while ((size_t)pwriter - (size_t)p < size)
+	{
+		*((unsigned char *)(pwriter)) = *((unsigned char *)(ptr + (pwriter - p)));
+		pwriter++;
+	}
+	while ((size_t)pwriter - (size_t)p < newsize)
+		*((unsigned char *)(pwriter++)) = 0;
+	free(ptr);
+	return (p);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char	*dstwriter;
+
+	if (!dest && !src)
+		return (0);
+	dstwriter = dest;
+	while (src++, dstwriter++, (size_t)dstwriter - 1 - (size_t)dest < n)
+		*(dstwriter - 1) = *(unsigned char *)(src - 1);
+	return (dest);
+}
+
+/*
+*	Search for specific byte inside *s memory pointer
+*	Return a pointer to the first occurence
+*/
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return ((void *)s + i);
+		i++;
+	}
+	return (0);
+}
+
+/*
+*	Concatenate src mem to dest mem
+*	Dest needs to have enough space
+*	Returns null if dest pointer is null
+*/
+void	*ft_memncat(void *dest, size_t start, void *src, size_t nb)
+{
+	int	i;
+
+	if (!dest)
+		return (0);
+	i = 0;
+	while ((unsigned int)i < nb)
+	{
+		((unsigned char *)dest)[start + i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
+
+/*
+*	Extend mem allocation to add the \0 char at the end
+*/
+char	*ft_memtostr(void *mem, size_t size)
+{
+	char	eos;
+
+	eos = '\0';
+	mem = ft_exallocf(mem, size, size + 1);
+	if (!mem)
+		return (0);
+	((unsigned char *)mem)[size] = eos;
+	return ((char *)mem);
+}
+
+
+#include "get_next_line.h"
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 3
+# define BUFFER_SIZE 4
 #endif
 
 #include <stdio.h>
@@ -29,6 +136,7 @@
 */
 ssize_t	fill_tab_with_lastbuffer(void **tab, ssize_t tabize, char *buffer, ssize_t buffersize)
 {
+	// need to do fill_tab_buffer_newline instead, wich will return next newline from buffer
 	ssize_t	size;
 	ssize_t	usedbytes_size;
 
@@ -49,7 +157,7 @@ ssize_t	fill_tab_with_lastbuffer(void **tab, ssize_t tabize, char *buffer, ssize
 *	Fill tab pointer with the next line
 *	Some buffersize don't work, need fix
 */
-size_t	fill_tab_with_nextline(void **tab, ssize_t size, int *fd)
+size_t	fill_tab_nextline(void **tab, ssize_t size, int *fd)
 {
 	static char		buffer[BUFFER_SIZE];
 	static ssize_t	rbytes;
@@ -94,7 +202,7 @@ char	*get_next_line(int fd)
 	output = 0;
 	p_output = &output;
 	tabsize = 0;
-	tabsize = fill_tab_with_nextline((void **)p_output, tabsize, &s_fd);
+	tabsize = fill_tab_nextline((void **)p_output, tabsize, &s_fd);
 	if (!tabsize)
 	{
 		free(output);
