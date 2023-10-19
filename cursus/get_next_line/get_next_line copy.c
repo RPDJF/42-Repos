53d446dfe7fb35d7a063c5a18035e3468fcc6915
Rui Paulo <86334233+RPDJF@users.noreply.github.com>
2023-10-19 14:26:00 +0200

    Seems to work

--- cursus/get_next_line/get_next_line copy.c
+++ cursus/get_next_line/get_next_line copy.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line copy.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rude-jes <ruipaulo.unif@outlook.fr>        +#+  +:+       +#+        */
+/*   By: rude-jes <rude-jes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 15:38:17 by rude-jes          #+#    #+#             */
-/*   Updated: 2023/10/19 11:40:07 by rude-jes         ###   ########.fr       */
+/*   Updated: 2023/10/19 14:25:37 by rude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
 /*
-* Returns a pointer to an extend malloc
-* Copies bytes from ptr to the returned malloc
-* ft_exallocf frees the ptr's pointed malloc
-*
-* Size inputs represents the size of the malloc in bytes
-* If ptr is null, returns a new 'calloc' with newsize
-* Returns 0 if allocation failed and free ptr
-*/
+ * Returns a pointer to an extend malloc
+ * Copies bytes from ptr to the returned malloc
+ * ft_exallocf frees the ptr's pointed malloc
+ *
+ * Size inputs represents the size of the malloc in bytes
+ * If ptr is null, returns a new 'calloc' with newsize
+ * Returns 0 if allocation failed and free ptr
+ */
 #include <stdio.h>
-void	*ft_exallocf(void *ptr, size_t size, size_t newsize)
+void *ft_exallocf(void *ptr, size_t size, size_t newsize)
 {
-	void	*p;
-	void	*pwriter;
+	void *p;
+	void *pwriter;
 
 	p = malloc(newsize);
 	if (!p)
@@ -51,10 +51,9 @@ void	*ft_exallocf(void *ptr, size_t size, size_t newsize)
 	return (p);
 }
 
-
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void *ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t	i;
+	size_t i;
 
 	if (!dest && !src)
 		return (0);
@@ -69,12 +68,12 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 }
 
 /*
-*	Search for specific byte inside *s memory pointer
-*	Return a pointer to the first occurence
-*/
-void	*ft_memchr(const void *s, int c, size_t n)
+ *	Search for specific byte inside *s memory pointer
+ *	Return a pointer to the first occurence
+ */
+void *ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
 	while (i < n)
@@ -87,13 +86,13 @@ void	*ft_memchr(const void *s, int c, size_t n)
 }
 
 /*
-*	Concatenate src mem to dest mem
-*	Dest needs to have enough space
-*	Returns null if dest pointer is null
-*/
-void	*ft_memncat(void *dest, size_t start, void *src, size_t nb)
+ *	Concatenate src mem to dest mem
+ *	Dest needs to have enough space
+ *	Returns null if dest pointer is null
+ */
+void *ft_memncat(void *dest, size_t start, void *src, size_t nb)
 {
-	int	i;
+	int i;
 
 	if (!dest)
 		return (0);
@@ -107,11 +106,11 @@ void	*ft_memncat(void *dest, size_t start, void *src, size_t nb)
 }
 
 /*
-*	Extend mem allocation to add the \0 char at the end
-*/
-char	*ft_memtostr(void *mem, size_t size)
+ *	Extend mem allocation to add the \0 char at the end
+ */
+char *ft_memtostr(void *mem, size_t size)
 {
-	char	eos;
+	char eos;
 
 	eos = '\0';
 	mem = ft_exallocf(mem, size, size + 1);
@@ -121,46 +120,16 @@ char	*ft_memtostr(void *mem, size_t size)
 	return ((char *)mem);
 }
 
-
 #include "get_next_line.h"
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 4
+#define BUFFER_SIZE 4
 #endif
 
 #include <stdio.h>
 
-/*
-*	When get_next_line is called a second time, the
-*	last buffer may contain unused bytes. This func
-*	should be called in this case to fill tab with
-*	unused bytes.
-*
-*	Returns tab size
-*	Returns 0/NULL if tabsize is 0
-*	Returns -1 if error
-*/
-ssize_t	fill_tab_with_lastbuffer(void **tab, ssize_t tabize, char *buffer, ssize_t buffersize)
+void *ft_submemf(void *s, size_t start, size_t len)
 {
-	// need to do fill_tab_buffer_newline instead, wich will return next newline from buffer
-	ssize_t	size;
-	ssize_t	usedbytes_size;
-
-	if (!buffersize)
-		return (0);
-	usedbytes_size = (char *)ft_memchr(buffer, '\n', buffersize) - buffer + 1;
-	if (buffersize == usedbytes_size)
-		return (0);
-	size = buffersize - usedbytes_size;
-	*tab = malloc(size);
-	if (!*tab)
-		return (-1);
-	ft_memncat(*tab, 0, buffer + usedbytes_size, size);
-	return (size + tabize);
-}
-
-void	*ft_submemf(void *s, size_t start, size_t len)
-{
-	void	*p;
+	void *p;
 
 	p = malloc(len);
 	if (!p)
@@ -169,62 +138,78 @@ void	*ft_submemf(void *s, size_t start, size_t len)
 	return (p);
 }
 
-size_t	fill_tab(void **tab, ssize_t tabsize, char *buffer, ssize_t *bsize)
+/*
+*	trouver \n dans buffer
+*	 si \n est prsent
+*	 		aggrandir tab et concat buffer jusqu'à \n
+*			décaler buffer avec memmove
+*			changer valeur de bsize
+*	 sinon
+*			aggrandir tab et concat tout buffer
+			bsize = 0
+*	 Retourne la taille du nouveau tableau & met a jour la taille de bsize
+*/
+size_t fill_frombuffer(void **tab, ssize_t size, char *buff, ssize_t *bsize)
 {
+	size_t new_size;
+	char *p_buff_nl;
 
-	// trouvé \n dans buffer
-	// si \n
-	// 		aggrandir tab et concat buffer jusqu'à \n
-	//		décaler buffer avec memmove
-	//		changer valeur de bsize
-	// sinon
-	//		aggrandir tab et concat tout buffer
-
-	size_t	new_tabsize;
-	char	*p_buffer_nl;
-
-	new_tabsize = tabsize;
-	p_buffer_nl = ft_memchr(buffer, '\n', *bsize);
-	if (p_buffer_nl)
+	new_size = size;
+	p_buff_nl = ft_memchr(buff, '\n', *bsize);
+	if (p_buff_nl)
 	{
-		new_tabsize += p_buffer_nl - buffer + 1;
-		ft_exallocf(*tab, tabsize, new_tabsize);
+		new_size += p_buff_nl - buff + 1;
+		*tab = ft_exallocf(*tab, size, new_size);
 		if (!*tab)
 			return (0);
-		ft_memncat(*tab, tabsize, buffer, new_tabsize - tabsize);
-		ft_memmove(buffer, p_buffer_nl + 1, new_tabsize - tabsize);
-		*bsize -= p_buffer_nl - buffer; // à corriger
+		ft_memncat(*tab, size, buff, new_size - size);
+		if (buff == p_buff_nl)
+			ft_memmove(buff, p_buff_nl + 1, *bsize - 1);
+		else if (p_buff_nl < buff + *bsize - 1)
+			ft_memmove(buff, p_buff_nl + 1, *bsize - (p_buff_nl - buff) - 1);
+		*bsize -= (p_buff_nl - buff) + 1;
+		return (new_size);
 	}
+	new_size += *bsize;
+	*tab = ft_exallocf(*tab, size, new_size);
+	if (!*tab)
+		return (0);
+	ft_memncat(*tab, size, buff, new_size - size);
+	*bsize = 0;
+	return (new_size);
 }
 
 /*
-*	Fill tab pointer with the next line
-*	Some buffersize don't work, need fix
-*/
-size_t	fill_tab_nextline(void **tab, ssize_t size, int *fd)
+ *	Fill tab pointer with the next line
+ */
+size_t fill_tab_nextline(void **tab, ssize_t size, int *fd)
 {
-	static char		buffer[BUFFER_SIZE];
-	static ssize_t	rbytes;
+	static char buffer[BUFFER_SIZE];
+	static ssize_t rbytes;
 
-	while (rbytes >= 0 && !ft_memchr(buffer, '\n', rbytes))
-	{
-		rbytes = read(fd, buffer, BUFFER_SIZE);
-		size = fill_tab(tab, size, buffer, rbytes);
-	}
+	if (rbytes)
+		size = fill_frombuffer(tab, size, buffer, &rbytes);
+	if (ft_memchr(*tab, '\n', size))
+		return (size);
+	rbytes = read(*fd, buffer, BUFFER_SIZE);
+	if (!rbytes)
+		return (size);
+	else
+		size = fill_tab_nextline(tab, size, fd);
 	return (size);
 }
 
 /*
-*	Returns the next line from a file as a string, containing \n.
-*	This functions use creates a static from fd, looping this
-*	functions sends each newline from last one, until EOF char.
-*/
-char	*get_next_line(int fd)
+ *	Returns the next line from a file as a string, containing \n.
+ *	This functions use creates a static from fd, looping this
+ *	functions sends each newline from last one, until EOF char.
+ */
+char *get_next_line(int fd)
 {
-	char		*output;
-	char		**p_output;
-	ssize_t		tabsize;
-	static int	s_fd;
+	char *output;
+	char **p_output;
+	ssize_t tabsize;
+	static int s_fd;
 
 	s_fd = fd;
 	output = 0;
@@ -245,11 +230,11 @@ char	*get_next_line(int fd)
 #include <fcntl.h>
 #include <stdio.h>
 
-int	main(void)
+int main(void)
 {
-	char	*line;
-	int		fd;
-	int		i;
+	char *line;
+	int fd;
+	int i;
 
 	i = -1;
 	fd = open("test.txt", O_RDONLY);
