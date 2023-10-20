04a8c4a56e4761e463c1617c4e40039ffcd0adb7
Rui Paulo <ruipaulo.unify@outlook.fr>
2023-10-20 20:19:21 +0200

    Updated libft

--- cursus/ft_printf/libs/libft.h
+++ cursus/ft_printf/libs/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rude-jes <rude-jes@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rude-jes <ruipaulo.unif@outlook.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 13:19:26 by rude-jes          #+#    #+#             */
-/*   Updated: 2023/10/20 13:24:22 by rude-jes         ###   ########.fr       */
+/*   Updated: 2023/10/20 20:13:27 by rude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,8 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 //	Check if is printable character
 int		ft_isprint(int c);
+//	Converts int number into a string with specific base
+char	*ft_itoa_base(int n, const char *base);
 //	Converts int number into a string
 char	*ft_itoa(int n);
 //	Returns a pointer to the first occurence of specific byte in memory s
@@ -130,7 +132,7 @@ void	*ft_exallocf(void *ptr, size_t size, size_t newsize);
  *	This functions use creates a static from fd, looping this
  *	functions sends each newline from last one, until EOF char.
  */
-char	*get_next_line(int fd);
+char	*ft_get_next_line(int fd);
 /*
  *	Concatenate src mem to dest mem
  *	Dest needs to have enough space
